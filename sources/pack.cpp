7038590f6a533ad92b0861709de18febff30de96
Kustov-Ilya <kustoff.il@yandex.ru>
2017-11-24 22:52:02 +0300

    Update pack.cpp

--- sources/pack.cpp
+++ sources/pack.cpp
@@ -6,31 +6,35 @@
 using json = nlohmann::json;
 
 
-struct ExamplePerson{
-	std::string  first_name = "Anton";
-	std::string  last_name = "Antonov";
+
+
+struct Email {
 	std::string nickname = "Antonio";
-	std::string server =  "mail";
-	size_t age = 37;
-	std::string phone = "8-999-999-00-00";
+	std::string server = "mail";
 };
 
-
+struct Person {
+	std::string  first_name = "Anton";
+	std::string  last_name = "Antonov";
+	Email        email;
+	size_t       age = 34;
+	std::string  phone = "8-999-999-00-00";
+};
 
 int main(int argc, char* argv[]) {
 	std::string File;
-	if(argc < 1)
+	if (argc < 1)
 		File = argv[1];
 	else {
 		std::cout << "You don't enter name of file";
 	}
-	ExamplePerson Ex;
+	Person Ex;
 	json Person;
 	std::ofstream ofst(File);
 	Person["first_name"] = Ex.first_name;
 	Person["last_name"] = Ex.last_name;
-	Person["email"]["nickname"] = Ex.nickname;
-	Person["email"]["server"] = Ex.server;
+	Person["email"]["nickname"] = Ex.email.nickname;
+	Person["email"]["server"] = Ex.email.server;
 	Person["age"] = Ex.age;
 	Person["phone"] = Ex.phone;
 	ofst << Person;
